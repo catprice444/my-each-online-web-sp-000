fbe6ad0a7e169e1a6af3f75a887565fa09cf681c
Catherine Price <catprice444@gmail.com>
2020-04-23 20:54:45 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,5 +2,11 @@
 # loop to iterate over each member of that array, yielding each
 # element contained in the array to a block.
 
-def my_each() 
+def my_each(words)
+  i = 0
+  while i < words.length
+    yield(words[i])
+    i = i + 1
+  end
+  words 
 end
